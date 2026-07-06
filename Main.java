4adda1b755da790e3ba6d9f5437a457fb935b8b9
demo <preethivelu90@gmail.com>
2026-07-06 20:51:55 +0530

    positive or negative number

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        int n=-5;
+        if(n>0){
+            System.out.print("positive number");
+        }
+        else if(n==0){
+            System.out.print("number 0");
+        }else{
+            System.out.print("negative number");
+        }
+    }
+}
