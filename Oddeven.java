aeb21e14575ebdd8914413219ffe4885edaa92ea
demo <preethivelu90@gmail.com>
2026-07-06 22:12:29 +0530

    programs

---
+++ Oddeven.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+        int n=18;
+       String status=n%2==0?"is even":"is odd";
+       System.out.print(n+status);
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,12 @@
+class Sum {
+    public static void main(String[] args) {
+        System.out.println("Start small. Ship something.");
+     int n1=12;
+     int n2=15;
+     int sum=0;
+   for(int i=n1;i<=n2;i++){
+       sum+=i;
+   }
+   System.out.print(sum);
+    }
+}
